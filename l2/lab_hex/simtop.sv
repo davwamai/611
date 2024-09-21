b1e5472862324229ccc33c2d281858abdba746f0
David Wamai <davwamai@gmail.com>
2024-09-21 12:14:42 -0400

    another sync

--- l2/lab_hex/simtop.sv
+++ l2/lab_hex/simtop.sv
@@ -6,6 +6,7 @@
 module simtop;
 
 	logic clk;
+  logic [17:0] SW;
 	logic [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
 
 	top dut
@@ -36,7 +37,26 @@ module simtop;
 		.HEX7(HEX7)
 	);
 
-// your code here
+  // period of 10 time units
+  initial clk = 0;
+  always #5 clk = ~clk; // Toggle every 5 time units
+
+  initial begin
+      // Init "switches" to 0
+      SW = 18'd0;
+
+      repeat (257) begin
+          #10;         // Stall 10 time units
+          SW = SW + 1;
+      end
+
+      #10 $finish;
+  end
+
+  initial begin
+      $display("Time\tSW\t\tHEX3 HEX2 HEX1 HEX0");
+      $monitor("%0t\t%h\t%h  %h  %h  %h", $time, SW[15:0], HEX3, HEX2, HEX1, HEX0);
+  end
 
 endmodule
 
