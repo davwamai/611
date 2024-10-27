ab7b6eb6d63334abd3c48ddd6e0d0a5f567a5ddb
davwamai <davwamai@gmail.com>
2024-10-27 16:30:42 -0400

    sync 1630

--- l3/lab_riu/simtop.sv
+++ l3/lab_riu/simtop.sv
@@ -62,6 +62,29 @@ module simtop;
       $display("D2: %8d", dut.cpu_inst.rf_inst.mem[18]);
       $display("D1: %8d", dut.cpu_inst.rf_inst.mem[9]);
       $display("D0: %8d", dut.cpu_inst.rf_inst.mem[8]);
+
+      // testcase all_instructions
+      $display("x1: %8d", dut.cpu_inst.rf_inst.mem[1]);
+      $display("x2: %8d", dut.cpu_inst.rf_inst.mem[2]);
+      $display("x3: %8d", dut.cpu_inst.rf_inst.mem[3]);
+      $display("x4: %8d", dut.cpu_inst.rf_inst.mem[4]);
+      $display("x5: %8d", dut.cpu_inst.rf_inst.mem[5]);
+      $display("x6: %8d", dut.cpu_inst.rf_inst.mem[6]);
+      $display("x7: %8d", dut.cpu_inst.rf_inst.mem[7]);
+      $display("x8: %8d", dut.cpu_inst.rf_inst.mem[8]);
+      $display("x9: %8d", dut.cpu_inst.rf_inst.mem[9]);
+      $display("x10: %8d", dut.cpu_inst.rf_inst.mem[10]);
+      $display("x11: %8d", dut.cpu_inst.rf_inst.mem[11]);
+      $display("x12: %8d", dut.cpu_inst.rf_inst.mem[12]);
+      $display("x13: %8d", dut.cpu_inst.rf_inst.mem[13]);
+      $display("x14: %8d", dut.cpu_inst.rf_inst.mem[14]);
+      $display("x15: %8d", dut.cpu_inst.rf_inst.mem[15]);
+      $display("x16: %8d", dut.cpu_inst.rf_inst.mem[16]);
+      $display("x17: %8d", dut.cpu_inst.rf_inst.mem[17]);
+      $display("x18: %8d", dut.cpu_inst.rf_inst.mem[18]);
+      $display("x19: %8d", dut.cpu_inst.rf_inst.mem[19]);
+      $display("x20: %8d", dut.cpu_inst.rf_inst.mem[20]);
+      $display("x21: %8d", dut.cpu_inst.rf_inst.mem[21]);
   end
 
 endmodule
